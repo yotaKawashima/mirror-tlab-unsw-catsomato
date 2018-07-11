af39ed03be8164288637d5d482376235e809217a
rannee <rannee@user-OptiPlex-9010.(none)>
2018-07-11 15:48:48 +1000

    Finalised figure 6 scripts (updated to latest working versions)

---
+++ figure_scripts/6-nontaggedHGP/plot_S2_hgpinspace.m
@@ -0,0 +1,131 @@
+% Plots spacial distribution of channels with mean high gamma power
+
+%% Set overall variables
+
+% PREREQUISITES:
+% Add the git repo to path.
+
+% directory where the drive is mounted. 
+disk_path = '/media/rannee/UNSW_Cat_Somatos/';
+
+% set figure path to be this temporary directory that is not on the main
+% script or figure paths (this is a directory where the old and broken
+% scripts are kept).
+fig_path = [];
+
+% highest level directory of data
+data_path = [disk_path 'data/'];
+
+% directory with the raw files
+raw_path = [data_path 'raw/'];
+
+% path to third party toolboxes
+tool_path = [disk_path 'scripts/thirdparty_toolboxes/'];
+chron_dir = [tool_path 'chronux/'];
+
+%% Script specific variables
+cat_name = 'C20110808_R03';
+data_dir = [data_path 'included_datasets/' cat_name '/epoched_rsampsl_biprref_evkresp_cmtspwr_evkdpwr_hgpcomp'];
+
+filename = [cat_name '_S2_FxxxAxxx_FxxxAxxx_epoched_rsampsl_biprref_evkresp_cmtspwr_evkdpwr_hgpcomp.mat'];
+
+%% load
+a = 2;
+load(filename)
+
+%% preproc
+% work out which channels to keep (the bipolar ones)
+if strcmp(data.label{1}(1:3), 'raw')
+    c1_ind = 1 + prod(data.custom.spatialconfig);
+    nChan = data.custom.nsignals - c1_ind + 1;
+else
+    nChan = data.custom.nsignals;
+    c1_ind = 1;
+end
+
+% frequency is already selected
+
+% grab extra variables
+labels = data.label(c1_ind:end);
+spatialconfigs = data.custom.spatialconfig;
+condfigs = data.custom.subplotconfig;
+
+%% loop through conditions
+for t = 1:numel(data.trial)
+    %% select the right channels
+    hgp_power = mean(data.trial{t}(c1_ind:end, :, :), 2);
+    
+    %% perform t-test
+    for k = 1:nChan
+        [allhs(k,1,t) , allps(k,1,t)]=ttest(hgp_power(k, 1, :), 0, 'Tail', 'right');
+        
+        
+    end
+    
+    %% find means
+    mean_hgp(:, :, t) = mean(hgp_power, 3);
+end
+
+
+
+%% threshold
+q = 0.05;
+
+mean_hgp(allhs==0)=NaN;
+
+% do FDR
+[pID, ~] = eeglab_fdr(allps(:, 1, :), q, 'parametric');
+tmp = mean_hgp(:, 1, :);
+tmp(allps(:, 1, :)>=pID)=NaN;
+
+mean_hgp(:, 1, :) = tmp;
+
+%% plot
+
+cmap = hot(64);
+cmap = cmap(60:-1:1, :);
+
+nAreas = numel(mean_hgp);
+[nChans, ~, nConds] = size(mean_hgp);
+
+
+clim = find_lims(mean_hgp);
+clim(2) = clim(2);
+
+    % relabel channels
+    relabels = draw_biprref_chlabfunction(labels);
+    
+
+
+        % make a figure
+        figure(1)
+        clf
+        set(gcf, 'Name', 'mean HGP S2') 
+        
+        for co = 1:nConds
+            % make subplot
+            subtightplot(condfigs(1), condfigs(2), co+1)
+            % goes across rows
+            
+            draw_biprref(mean_hgp(:,1,co), relabels, spatialconfigs, clim)
+            colormap(cmap)
+        end
+        
+        % call function to clean up and label the plot
+        subtightplotcleaner(1, condfigs, 'cleanticks', false, ...
+            'catnames', [{'F023A000_F200A000'}, data.datalabels], ...
+            'topinds', 10:17, 'sideinds', 1:8, 'box', false)
+        
+        dt = datestr(now, 'yyyymmdd');
+        
+        % save figure
+         print(gcf, '-depsc', ['Figure7_S2_' dt])
+
+
+
+
+figure(2)
+colorbar
+colormap(cmap)
+caxis(clim)
+print(gcf, '-depsc', ['Figure7_colorbar_' dt])
--- figure_scripts/6-nontaggedHGP/plot_fstat_allfreq.m
+++ figure_scripts/6-nontaggedHGP/plot_fstat_allfreq.m
@@ -1,7 +1,30 @@
-% adapted from /media/rannee/UNSW_Cat_Somatos/scripts/28Jul17/fstat_fig4_plotter.m
+% Plots the distribution of f-statistic from ANOVA over 0-250 Hz
+
+%% Set overall variables
+
+% PREREQUISITES:
+% Add the git repo to path.
+
+% directory where the drive is mounted. 
+disk_path = '/media/rannee/UNSW_Cat_Somatos/';
+
+% set figure path to be this temporary directory that is not on the main
+% script or figure paths (this is a directory where the old and broken
+% scripts are kept).
+fig_path = [];
+
+% highest level directory of data
+data_path = [disk_path 'data/'];
+
+% directory with the raw files
+raw_path = [data_path 'raw/'];
+
+% path to third party toolboxes
+tool_path = [disk_path 'scripts/thirdparty_toolboxes/'];
+chron_dir = [tool_path 'chronux/'];
 
 % data directory
-data_dir = '/media/rannee/UNSW_Cat_Somatos/data/collated_data/anoved_rsampsl_biprref_evkresp_cmtspwr_adatout_fstonly';
+data_dir = [data_path 'collated_data/anoved_rsampsl_biprref_evkresp_cmtspwr_adatout_fstonly'];
 
 
 %% by area
