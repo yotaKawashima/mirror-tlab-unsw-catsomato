f4e7c645a9f1647d7702bce4adbdb0d214eed588
Rannee Li <rli68@student.monash.edu>
2018-07-16 00:39:53 +0000

    Cleaned, added comments

--- figure_scripts/7-HGPtimecourse/plottimecourse.m
+++ figure_scripts/7-HGPtimecourse/plottimecourse.m
@@ -1,28 +1,37 @@
+% select area
 S = '2';
-load(['C20110808_R03_S' S '_F023A159_F200A016_epoched_rsampsl_biprref_evkresp_cmtsgrm.mat'])
+
+% load data
+load(['C20110808_R03_S' S '_F023A159_F200A016_epoched_rsampsl_biprref_cmtsgrm.mat'])
+
+% get date for saving the output files
 dt = datestr(now, 'yyyymmdd');
 
-
+% grab power data, mean across trials
 p_tmp = data.trial;
-p_tmp = mean(p_tmp, 3);
+p_tmp = mean(p_tmp, 3); % channels x frequency x trials x timesteps
+
+% find frequency indices
 [~, in] = find_closest(data.freq{1}, 100);
 [~, i50] = find_closest(data.freq{1}, 50);
 [~, i150] = find_closest(data.freq{1}, 150);
+f_inds = i50+5:i150-5; % only concerned with the high gamma band
 
-f_inds = i50+5:i150-5;
-
+% Find out which channels would be good
 figure(1); shg
 imagesc(permute(mean(p_tmp(:, f_inds, :, :), 2), [1, 4, 2, 3]))
 
+% Set channels to plot
 if S == '1'
 	chs = [174, 183, 235, 134];
 else
-    chs =[14, 34, 35, 94, 101, 102]+64;%108;%[100, 125, 126, 96, 113];
+    chs =102;%108;%[100, 125, 126, 96, 113];
 end
 
+% Plot for each channel
 for k = 1:numel(chs)
     figure(2)
-    imagesc(data.freq_t, data.freq{1}(f_inds), permute(p_tmp(chs(k), f_inds, :, :), [2, 4, 1, 3])); 
+    imagesc(data.freq_t+data.custom.time(1), data.freq{1}(f_inds), permute(p_tmp(chs(k), f_inds, :, :), [2, 4, 1, 3])); 
     set(gca, 'ydir', 'normal'); 
     
     xlabel('time (s)')
@@ -33,7 +42,8 @@ for k = 1:numel(chs)
     
     shg
     
-    print(gcf, '-dpng', ['TimeFreqFig_C20110808_R03_S' S '_Ch' num2str(chs(k), '%03i') '_' dt])
-    pause()
+    % Save to file
+    %print(gcf, '-dpng', ['TimeFreqFig_C20110808_R03_S' S '_Ch' num2str(chs(k), '%03i') '_' dt])
+    %pause() % Wait for manual verification
 end
 
