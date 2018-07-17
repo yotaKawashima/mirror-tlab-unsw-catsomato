ebeba35b1ce39e51500ecb3b5c3b367e656185aa
rannee <rannee@user-OptiPlex-9010.(none)>
2018-07-17 16:57:17 +1000

    Added ability to load from file or run extra files

--- figure_scripts/2-powerspectrums/call_powerplots.m
+++ figure_scripts/2-powerspectrums/call_powerplots.m
@@ -1,21 +1,61 @@
-data_dir = '/media/rannee/UNSW_Cat_Somatos/data/collated_data';
+run(fullfile(mfilename('fullpath'), '../../path_setup.m'))
+
+data_dir = fullfile(data_path, 'included_datasets');
+premade_data = fullfile(data_path, 'collated_data', 'powerplots_data');
 
 imgformat = '-depsc';
 
+cat_names = dirsinside(data_dir);
 
-% power
-data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr_avtrcon/';
-collectallcats(data_dir, data_type, 'plotmean', 1)
+%% power
+data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr';
+fname = dir(fullfile(premade_data, ['*' data_type '.mat']));
+if ~isempty(fname)
+    collectallcats(premade_data, data_type, 'plotmean', 1, 'fromfile', fname.name)
+else
+    collectallcats(data_dir, data_type, 'plotmean', 1, 'domain', [0, 250])
+end
 print(1, imgformat, 'Figure2A')
 
-% SNR
-data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr_snrsurr_avtrcon/';
-collectallcats(data_dir, data_type, 'plotmean', 2, 'plotttest', 4)
+%% SNR
+data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr_snrsurr';
+fname = dir(fullfile(premade_data, ['*' data_type '.mat']));
+if ~isempty(fname)
+    collectallcats(premade_data, data_type, 'plotmean', 2, 'plotttest', 4, 'fromfile', fname.name)
+else
+    % check if snrsurr files exist
+    for c = 1:numel(cat_names)
+        loadnames = dir(fullfile(data_dir, cat_names{c}, data_type, '*.mat'));
+
+        % check if the data files exist! if not, make them.
+        if isempty(loadnames)
+            call_snrtosurrounds(data_path, data_type(1:end-8), cat_names{c})
+        end
+    end
+    
+    collectallcats(data_dir, data_type, 'plotmean', 2, 'plotttest', 4)
+end
 print(2, imgformat, 'Figure2B')
 print(4, imgformat, 'Figure2D')
 
-% EP
-data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr_evkdpwr_avtrcon/';
-collectallcats(data_dir, data_type, 'plotmean', 3, 'plotttest', 5)
+%% EP
+data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr_evkdpwr';
+fname = dir(fullfile(premade_data, ['*' data_type '.mat']));
+if ~isempty(fname)
+    collectallcats(premade_data, data_type, 'plotmean', 3, 'plotttest', 5, 'fromfile', fname.name)
+else
+    
+    % check if snrsurr files exist
+    for c = 1:numel(cat_names)
+        loadnames = dir(fullfile(data_dir, cat_names{c}, data_type, '*.mat'));
+
+        % check if the data files exist! if not, make them.
+        if isempty(loadnames)
+            call_evokedpower(data_path, data_type(1:end-8), cat_names{c})
+        end
+    end
+    
+    collectallcats(data_dir, data_type, 'plotmean', 3, 'plotttest', 5)
+end
 print(3, imgformat, 'Figure2C')
 print(5, imgformat, 'Figure2E')
