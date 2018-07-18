b9f0afd3ae3c9cb4419fbe9860bc7a9179f8896a
rannee <rannee@user-OptiPlex-9010.(none)>
2018-07-18 16:20:37 +1000

    Added / to end of filepaths due to afpc_filemover stripping the last character

--- figure_scripts/4-SNRpatternS2/call_snrtosurrounds.m
+++ figure_scripts/4-SNRpatternS2/call_snrtosurrounds.m
@@ -1,7 +1,7 @@
 function call_snrtosurrounds(data_path, datatype_in, cat_name)
 
 data_dir = fullfile(data_path, 'included_datasets', cat_name, '/');
-dir_sec = fullfile(data_dir, datatype_in);
+dir_sec = fullfile(data_dir, datatype_in, '/');
 
 loadname = dir(fullfile(dir_sec, [cat_name, '*.mat']));
 
