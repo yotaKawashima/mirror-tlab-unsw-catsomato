2253a58a7ae302512a8db26f61b2de415832fa47
rannee <rannee@user-OptiPlex-9010>
2019-01-21 10:22:46 +1100

    Undocumented changes

--- figure_scripts/2-powerspectrums/power_types.m
+++ figure_scripts/2-powerspectrums/power_types.m
@@ -5,4 +5,4 @@ data_type = 'epoched_rsampsl_biprref_evkresp_cmtspwr';
 
 filename = dir(fullfile('./', ['*' data_type '.mat']));
 
-data = powerplot_data(data_dir, data_type, filename)
+data = powerplot_data(data_dir, data_type, filename);
