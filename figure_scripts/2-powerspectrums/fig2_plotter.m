ba3369672dfef3cd5078ac06d69a35128c8fc3c2
rannee <rannee@user-OptiPlex-9010>
2019-01-29 15:21:26 +1100

    Added figure printing

--- figure_scripts/2-powerspectrums/fig2_plotter.m
+++ figure_scripts/2-powerspectrums/fig2_plotter.m
@@ -10,7 +10,7 @@ data_dir = fullfile(data_path, 'included_datasets');
 save_dir = fullfile(data_path, 'collated_data', 'fig2_data');
 
 % output image type
-imgtype = '-depsc';
+imgtype = '-dpng';
 
 % Current date and time for output image
 dt = datestr(now, 'yyyymmdd');
@@ -20,12 +20,21 @@ max_only = true;
 fmax = 250;
 reload = false;
 
+if max_only
+    m = 'max_';
+else
+    m = '';
+end
+
 %% A: log power.
 
 [data, ~] = f2p_loader(data_dir, save_dir, 'epoched_rsampsl_biprref_evkresp_cmtspwr', max_only, reload, false, fmax);
 
 figure(1); clf
 plot(data.freq{1}, nanmean(data.trial, 1))
+xlabel('frequency')
+ylabel('power')
+print(gcf, imgtype, ['Fig2a_' m dt])
 
 %% B/D: SNR
 
@@ -33,9 +42,15 @@ plot(data.freq{1}, nanmean(data.trial, 1))
 
 figure(2); clf
 plot(data.freq{1}, nanmean(data.trial, 1))
+xlabel('frequency')
+ylabel('SNR')
+print(gcf, imgtype, ['Fig2b_' m dt])
 
 figure(3); clf
 plot(ttest_data.freq{1}, ttest_data.tstats)
+xlabel('frequency')
+ylabel('t-statistic')
+print(gcf, imgtype, ['Fig2c_' m dt])
 
 %% C/E: EP
 
@@ -43,6 +58,12 @@ plot(ttest_data.freq{1}, ttest_data.tstats)
 
 figure(4); clf
 plot(data.freq{1}, nanmean(data.trial, 1))
+xlabel('frequency')
+ylabel('evoked power')
+print(gcf, imgtype, ['Fig2d_' m dt])
 
 figure(5); clf
 plot(ttest_data.freq{1}, ttest_data.tstats)
+xlabel('frequency')
+ylabel('t-statistic')
+print(gcf, imgtype, ['Fig2e_' m dt])
